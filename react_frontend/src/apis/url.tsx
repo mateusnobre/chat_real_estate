21ae053a6e51115cf5079c22006f705c42b2b94c
mateusnobre <mateusnobrests@gmail.com>
2023-05-10 11:52:58 -0300

    update url

--- react_frontend/src/apis/url.tsx
+++ react_frontend/src/apis/url.tsx
@@ -1,7 +1,7 @@
 
 var url = '';
 if (process.env.NODE_ENV === 'production') {
-    url = 'http://https://chat-real-estate-backend.onrender.com/5601';
+    url = 'https://chat-real-estate-backend.onrender.com/';
 }
 else if (process.env.NODE_ENV === 'development') {
     url = 'http://localhost:5601';
